ae50121785445582e5f14c6db4c552d09f24ffb1
NiveditaV96 <43562959+NiveditaV96@users.noreply.github.com>
2019-05-02 11:33:01 +0530

    Abstractclass with .gitignore

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    public abstract class AC
+    {
+        public AC(int a, int b)
+        {
+
+        }
+
+        public abstract void Foo();
+
+        public void Sum(int a, int b)
+        {
+            
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
